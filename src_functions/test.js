80b291692cdecd1fe972cc5b98a46a4dd8d58cbf
Louis G <lg637@hmgcc.gov.uk>
2019-05-17 13:42:39 +0100

    New Function

---
+++ src_functions/test.js
@@ -0,0 +1,8 @@
+// src_function/test.js
+exports.handler = function(event, context, callback) {
+    console.log('hello world')
+    callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+    });
+}
