d8036b63773efd41df7e1e232064de05e8443dd9
Louis G <lg637@hmgcc.gov.uk>
2019-05-17 14:56:13 +0100

    skynet

--- src_functions/test.js
+++ src_functions/test.js
@@ -1,8 +1,23 @@
-// src_function/test.js
+import trainedNet from './neuralNet/trained-net';
+
 exports.handler = function(event, context, callback) {
-    console.log('hello world')
+    const board = JSON.parse(event.body)
+    const result = trainedNet(board)
+    
+    console.log(result)
+
+    const emptySpaces = board.map((space, index) => {
+        return space === 0 ? result[index] : null
+    }).filter(space => {
+        return space !== null
+    })
+
+    const response = {
+        index: result.indexOf(emptySpaces.sort(function(a, b){return b - a})[0])
+    }
+  
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-    });
-}
+        statusCode: 200,
+        body: JSON.stringify(response)
+    })
+  }
